42f98db797773ffcedfb767efb11fc02f2ed4c76
arianna choza <unachoza@gmail.com>
2020-03-01 16:33:09 -0500

    landing page

--- src/Components/Progress-Bar/Progress-Bar.component.js
+++ src/Components/Progress-Bar/Progress-Bar.component.js
@@ -7,13 +7,13 @@ const ProgressBar = () => {
 <section>
 
   <ol class="progress-bar">
-    <li class="is-complete"><span>Question 1</span></li>  
-    <li class="is-complete"><span>Question 2</span></li>  
-    <li class="is-complete"><span>Question 3</span></li>
-    <li class="is-complete"><span>Question 4</span></li>  
-    <li class="is-complete"><span>Question 5</span></li>
-    <li class="is-complete"><span>Question 6</span></li>
-    <li class="is-active"><span>Question 7</span></li>  
+    <li class="is-active"><span>Question 1</span></li>  
+    <li><span>Question 2</span></li>  
+    <li><span>Question 3</span></li>
+    <li><span>Question 4</span></li>  
+    <li><span>Question 5</span></li>
+    <li><span>Question 6</span></li>
+    <li><span>Question 7</span></li>  
     <li><span>Question 8</span></li>
     <li><span>Question 9</span></li>
     <li><span>Complete</span></li>  
